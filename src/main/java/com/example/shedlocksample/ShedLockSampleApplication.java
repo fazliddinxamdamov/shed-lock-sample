8624d7ea2b712847de5bb35de30697468cb51651
Fazliddin <fazliddinxamdamov102@gmail.com>
2022-10-07 11:31:43 +0500

    Initial commit

---
+++ src/main/java/com/example/shedlocksample/ShedLockSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.shedlocksample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShedLockSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShedLockSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shedlocksample/ShedLockSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shedlocksample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShedLockSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
